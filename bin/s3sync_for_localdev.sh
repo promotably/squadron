2ebf7185d6768c9c8693e65eba7ae6e9008c3bc9
Vincent Rivellino <var@promotably.com>
2015-03-03 19:27:13 -0500

    Shouldn't need to sync db/ anymore

--- bin/s3sync_for_localdev.sh
+++ bin/s3sync_for_localdev.sh
@@ -13,5 +13,4 @@ set -ex
 
 SRC_CI=jenkins
 aws s3 sync --delete s3://promotably-build-artifacts/$SRC_CI/ s3://$destbucket/localdev/
-aws s3 sync --delete s3://promotably-build-artifacts/db/ s3://$destbucket/db/
 aws s3 sync --delete s3://promotably-build-metadata/validated-builds/$SRC_CI/ s3://$destbucket/validated-builds/localdev/
